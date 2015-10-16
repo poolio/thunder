a077caa939296c66bf8ed949a0961289d410e4d6
Ben Poole <benmpoole@gmail.com>
2015-10-16 13:28:29 -0700

    add missing utils

--- thunder/registration/methods/utils.py
+++ thunder/registration/methods/utils.py
@@ -123,3 +123,175 @@ def computeDisplacement(arry1, arry2):
     adjusted = [int(d - n) if d > n // 2 else int(d) for (d, n) in pairs]
 
     return adjusted
+
+def zeroBorder(vol, border=1, cval=0.0):
+    """Zero-out boundary voxels of a volume.
+
+    Parameters
+    ----------
+        vol: 3d volume
+        border: scalar or tuple containing borders for each dimension
+        cval: constant value to replace boundary voxels with, default is 0.0
+    """
+    import numpy as np # sorry
+    vol = vol.copy() # create new copy so we don't overwrite vol
+    dims = np.array(vol.shape)
+    if np.size(border) == 1:
+        border = border * np.ones(vol.ndim, dtype=int)
+    border = np.array(border, dtype=int)
+    # Don't apply border to singleton dimensions.
+    border[dims == 1] = 0
+    assert len(border) == vol.ndim
+    if np.any(dims - border <= 0):
+        raise ValueError('Border %s exceeds volume shape %s.' %
+                (str(border), str(dims)) )
+    for dim, bval in enumerate(border):
+        if bval > 0:
+            slices = [slice(bval) if d == dim else slice(None) for d in xrange(vol.ndim)]
+            vol[slices] = cval
+            slices[dim] = slice(-bval, None)
+            vol[slices] = cval
+    return vol
+
+
+def solveLinearized(vec, jacobian, reference, robust=False):
+    """Solve linearized registration problem for change in transformation parameters and weighting on reference basis.
+
+    Parameters
+    ----------
+    vec : array, shape (nvoxels,)
+        vectorized volume
+    jacobian: array, shape (nvoxels, nparams)
+        jacobian for each transformation parameter
+    reference: array, shape (nvoxels, nbasis)
+        array of vectorized reference volumes
+    robust: bool, optional, default = False
+        solve a least absolute deviation problem instead of least squares
+
+    Returns
+    -------
+    deltaTransforms : array, shape (nparams,)
+        optimal change in transformation parameters
+    coeff : array, shape (nbasis,)
+        optimal weighting of reference volumes
+    """
+    from numpy import column_stack
+    A = column_stack((jacobian, reference))
+    if robust:
+        from statsmodels.regression.quantile_regression import QuantReg
+        quantile = 0.5
+        model = QuantReg(vec, A).fit(q=quantile)
+        params = model.params
+    else:
+        from numpy.linalg import lstsq
+        model = lstsq(A, vec)
+        params = model[0]
+    from numpy import split
+    deltaTransform, coeff = split(params, [jacobian.shape[1]])
+    return deltaTransform, coeff
+
+
+def volToVec(vol):
+    """
+    Convert volume to vector.
+
+    Parameters
+    ----------
+    vol : array
+        volume
+
+    Returns
+    -------
+    vectorized volume
+    """
+    return vol.ravel()
+
+
+def vecToVol(vec, dims):
+    """
+    Convert vector to volume.
+
+    Parameters
+    ----------
+    vec : array, shape (nvoxels,)
+        vectorized volume
+    dims : tuple, optional
+        shape of volume, must be set to reshape vectors into volumes
+
+    Returns
+    -------
+    volume if input is a vector, and vector if input is a volume
+    """
+    assert vec.size == prod(dims)
+    return vec.reshape(dims)
+
+
+def volumesToMatrix(vols):
+    """Convert list of volumes to a matrix.
+
+    Parameters
+    ----------
+    vols : list of arrays
+
+    Returns
+    -------
+    array with size nvoxels by number of volumes
+    """
+    from numpy import column_stack
+    if not isinstance(vols, list):
+        return volToVec(vols)
+    else:
+        return column_stack(map(volToVec, vols)).squeeze()
+
+
+def imageGradients(im, sigma=None):
+    """Compute gradients of volume in each dimension using a Sobel filter.
+
+    Parameters
+    ----------
+    im : ndarray
+        single volume
+    sigma : float or tuple, optional, default = None
+        smoothing amount to apply to volume before computing gradients
+    """
+    from scipy.ndimage.filters import gaussian_filter, sobel
+    if sigma is not None:
+        im = gaussian_filter(im, sigma)
+    grads = [sobel(im, axis=dim, mode='constant') / 8.0 for dim in xrange(im.ndim)]
+    return grads
+
+
+def imageJacobian(vol, tfm, grid=None, sigma=None, normalize=True, border=1, order=1):
+    """Compute Jacobian of volume w.r.t. transformation parameters
+
+    Args:
+        vol: volume
+        tfm: Transform object
+        sigma: smoothing bandwidth for gradients (None for no smoothing)
+        normalize: Whether to normalize images before aligning.
+        border: Number or tuple of border sizes to zero after transforming.
+        order: interpolation order used by map_coordinates
+    Returns:
+        tvol : array
+            transformed volume
+        jacobianVols : list of arrays
+            list of volume Jacobians, one for each parameter of the transformation
+    """
+    from numpy.linalg import norm
+    if grid is None:
+        from thunder.imgprocessing.transformation import GridTransformer
+        grid = GridTransformer(vol.shape)
+    grads = imageGradients(vol, sigma)
+    tvol = zeroBorder(tfm.apply(vol, grid, order=order))
+    normVol = norm(tvol.ravel())
+    if normVol == 0.0:
+        raise ValueError('Transform yields volume of zeroes.')
+    grads = [zeroBorder(tfm.apply(grad, grid, order=order)) for grad in grads]
+    if normalize:
+        if normVol == 0.0:
+            raise ValueError('Transform yields volume of zeroes.')
+        # Update gradients to reflect normalization
+        grads = [grad / normVol - (grad * tvol).sum() / (normVol**3) * tvol for grad in grads]
+        tvol /= normVol
+    jacobianVols = tfm.jacobian(grads, grid.homo_points)
+    return tvol, jacobianVols
